72f23d9ef45e0a186835b468144a11759d32b635
王凤祥 <1205679862@qq.com>
2024-01-23 08:55:30 +0800

    feat: 初始项目

---
+++ ingest-data.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: 摄取数据
+ * @Author: wangfengxiang
+ * @Date: 2024-01-23 08:32:22
+ * @LastEditTime: 2024-01-23 08:53:13
+ * @LastEditors: wangfengxiang
+ */
+import {UnstructuredLoader} from 'langchain/document_loaders/fs/unstructured'
